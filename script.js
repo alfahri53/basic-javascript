b9935119585f66fb3041f0fd3d29475064506678
alfahri53 <alfahrinurhidayat27@gmail.com>
2020-11-21 10:12:31 +0700

    latih-javascript

---
+++ script.js
@@ -0,0 +1,108 @@
+//console.log("Hello, World!!")
+
+document.getElementById('heading').innerHTML = "Hello World!!"
+
+/* variable */
+
+//var name = "Alfahri";
+//console.log(name);
+//
+//var lastname = "NurHidayat";
+//console.log(lastname);
+//
+//var age = 19;
+//console.log(age);
+//
+//var fullName = name + " " + lastname;
+//console.log(fullName);
+//
+//var fullName = true;
+//console.log(fullName);
+//
+//var job = "Trainer";
+//console.log(job);
+//
+//var bio = "Nama saya " + name + " " + lastname + " Umur saya " + age + " " + "Pekerjaan saya adalah " + job;
+//
+//console.log(bio);
+
+var x = 5;
+var y = 7;
+var z = x + y;
+console.log(z);
+
+var x = 5;
+var y = 2;
+var z = x *  y;
+console.log(z);
+
+// macam operasi aritmatika
+
+//+
+//-
+///
+//*
+//%
+//++ -->increment
+//-- -->decrement
+
+// assigmment operator
+
+//operator contoh   sama seperti
+//=       x = y     x = y
+//+=      x += y    x = x + y
+//-=      x -= y    x = x - y
+//*=      x *= y    x = x * y
+///=      x /= y    x = x / y
+//%=      x %= y    x = x % y
+
+//var x = 10;
+//x += 5;
+//console.log(x);
+
+// string operator 
+
+//text1 = "Alfahri";
+//text2 = "NurHidayat";
+//text3 = text1 + " " + text2;
+//console.log(text3);
+
+//x = 5 + 5;
+//y = "5" + 5;
+//z = "hello" + 5;
+//console.log(x);
+//console.log(y);
+//console.log(z);
+
+// comparrison operator
+
+//==     sama dengan
+//===    nilai yang sama dan tipe yang sama
+//!=     tidak sama 
+//!==    tidak sama nilainya atau tidak sama tipe
+//>      lebih besar dari
+//<      kurang dari
+//>=     lebih besar sama dengan
+//<=     kurang dari sama dengan
+//?      ternary operator
+
+// logical operator 
+
+//&&  logical NamedNodeMap
+//||  logical or 
+//!   logical not
+
+// type operator
+
+//typeof  return --> tipe 
+//instanceof return --> Boolean
+
+// bitwise 
+
+& and 
+| or 
+~ Not 
+^ xor 
+<< leftshift
+>> rightshift
+>>> unsigned rightshfit
